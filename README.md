0a02120c237c03b63a035a12cdffb153ea7a9406
yamashun <yshunske@gmail.com>
2018-07-08 23:59:02 +0900

    iterator:具体的な集合体(例ではarticel)の実装に依存せずにスキャン処理を実装できることがメリット

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # design_pattern_training
+以下のブログの内容を写経していく
+https://morizyun.github.io/ruby/design-pattern-index.html
---
+++ iterator/blog.rb
@@ -0,0 +1,21 @@
+class Blog
+  def initialize
+    @articles = []
+  end
+
+  def get_article_at(index)
+    @articles[index]
+  end
+
+  def add_article(article)
+    @articles << article
+  end
+
+  def length
+    @articles.length
+  end
+
+  def iterator
+    BlogIterator.new(self)
+  end
+end
